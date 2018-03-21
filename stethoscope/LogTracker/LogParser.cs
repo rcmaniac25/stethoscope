8c34ee7266bf3d683597317ce4c7e885198892ca
Rcmaniac25 <rcmaniac25@hotmail.com>
2018-03-21 02:46:01 -0400

    Initial work on a XML-based log parser

---
+++ stethoscope/LogTracker/LogParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace LogTracker
+{
+    public class LogParser
+    {
+        public bool HandleNewXmlElement(XElement element)
+        {
+            //TODO
+            return false;
+        }
+    }
+}
--- stethoscope/LogTracker/Program.cs
+++ stethoscope/LogTracker/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.Xml;
 
 namespace LogTracker
 {
@@ -9,6 +9,42 @@ namespace LogTracker
     {
         public static void Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("Need a XML document to load of log info");
+                return;
+            }
+
+            var parser = new LogParser();
+
+            using (var ms = new MemoryStream())
+            {
+                var rootElementStringBytes = Encoding.UTF8.GetBytes("<root>");
+                ms.Write(rootElementStringBytes, 0, rootElementStringBytes.Length);
+
+                //TODO: this doesn't work for streaming, but read/write streams don't really work/exist right now
+                using (var fr = new FileStream(args[0], FileMode.Open))
+                {
+                    fr.CopyTo(ms);
+                }
+
+                //XXX: not going to show up in streaming log
+                var rootEndElementStringBytes = Encoding.UTF8.GetBytes("</root>");
+                ms.Write(rootEndElementStringBytes, 0, rootEndElementStringBytes.Length);
+
+                ms.Position = 0L;
+                using (var xmlReader = XmlReader.Create(ms))
+                {
+                    while (xmlReader.Read())
+                    {
+                        //TODO
+                        if (!parser.HandleNewXmlElement(null))
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
