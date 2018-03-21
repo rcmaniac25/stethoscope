96897d0142b9536506f2426ef06fe6c8329b113b
Rcmaniac25 <rcmaniac25@hotmail.com>
2018-03-21 02:12:13 -0400

    Switched from C++ to C# for initial design/testing phase

---
+++ stethoscope/LogTracker/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LogTracker")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c401a78e-34f2-4d82-aab4-9fe8bd8fe44a")]
--- stethoscope/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <string>
-
-int main(int argc, char* argv[])
-{
-	//TODO
-	return 0;
-}
