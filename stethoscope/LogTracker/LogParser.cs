54f66b49d3d1f1870fa77dedd4899c14aaec79c7
Rcmaniac25 <rcmaniac25@hotmail.com>
2018-03-23 00:48:36 -0400

    Initial organization of logs

--- stethoscope/LogTracker/LogParser.cs
+++ stethoscope/LogTracker/LogParser.cs
@@ -1,19 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Xml;
 using System.Xml.Linq;
 
 namespace LogTracker
 {
+    public class FunctionLog
+    {
+        public string Name { get; private set; }
+        public IList<string> Logs { get; private set; }
+
+        public FunctionLog(string name)
+        {
+            Name = name;
+            Logs = new List<string>();
+        }
+    }
+
+    public class FileLog
+    {
+        public string Path { get; private set; }
+        public IList<FunctionLog> Functions { get; private set; }
+
+        private string lastFunction;
+
+        public FileLog(string path)
+        {
+            Path = path;
+            Functions = new List<FunctionLog>();
+        }
+
+        public void AddLog(string function, string log)
+        {
+            if (lastFunction != function)
+            {
+                Functions.Add(new FunctionLog(function));
+                lastFunction = function;
+            }
+            Functions[Functions.Count - 1].Logs.Add(log);
+        }
+    }
+
+    public class ThreadLog
+    {
+        public string ThreadID { get; private set; }
+        public IList<FileLog> Files { get; private set; }
+
+        private string lastFile;
+        
+        public ThreadLog(string id)
+        {
+            ThreadID = id;
+            Files = new List<FileLog>();
+        }
+
+        public void AddLog(string file, string function, string log)
+        {
+            if (lastFile != file)
+            {
+                Files.Add(new FileLog(file));
+                lastFile = file;
+            }
+            Files[Files.Count - 1].AddLog(function, log);
+        }
+    }
+
     public class LogParser
     {
+        private Dictionary<string, ThreadLog> logThreads = new Dictionary<string, ThreadLog>();
+
         public bool HandleXmlElement(XElement element)
         {
-            Console.WriteLine($"=> {element.Attribute("src").Value}:{element.Attribute("sln").Value} - {element.Attribute("fun").Value}");
+            //Console.WriteLine($"=> {element.Attribute("src").Value}:{element.Attribute("sln").Value} - {element.Attribute("fun").Value}");
+
+            //XXX while logs shouldn't be out of order, it's possible
+
+            var threadId = element.Attribute("thrd").Value;
+            if (!logThreads.ContainsKey(threadId))
+            {
+                logThreads.Add(threadId, new ThreadLog(threadId));
+            }
+
+            var thread = logThreads[threadId];
+            thread.AddLog(element.Attribute("src").Value, element.Attribute("fun").Value, (element.LastNode as XCData).Value);
+
             //TODO
             return true;
+        }
+
+        public void ResetLogs()
+        {
+            logThreads.Clear();
+        }
+        
+        private static string GenerateIndentLog(int indent)
+        {
+            return new string(' ', indent * 2); //XXX config for indent size
+        }
+
+        public void PrintTrace()
+        {
+            foreach (var thread in logThreads.Values)
+            {
+                int indent = 0;
+                Console.WriteLine($"Thread {thread.ThreadID}");
+                foreach (var file in thread.Files)
+                {
+                    foreach (var function in file.Functions)
+                    {
+                        Console.WriteLine($"Start {function.Name} // {file.Path}");
+
+                        indent++;
+                        foreach (var log in function.Logs)
+                        {
+                            Console.WriteLine($"{GenerateIndentLog(indent)}{log}");
+                        }
+                        indent--;
+
+                        Console.WriteLine($"End {function.Name}");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- stethoscope/LogTracker/Program.cs
+++ stethoscope/LogTracker/Program.cs
@@ -101,6 +101,8 @@ namespace LogTracker
                     }
                 }
             }
+            
+            parser.PrintTrace();
         }
     }
 }
