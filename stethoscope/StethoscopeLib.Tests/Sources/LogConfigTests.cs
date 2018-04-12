0f52c662f8fa8e5bd35d0871f0184e39049ad374
Rcmaniac25 <rcmaniac25@hotmail.com>
2018-04-12 00:39:29 -0400

    Upgraded to VS 2017
    Added a test project and got tests to actually run

--- stethoscope/StethoscopeLib.Tests/Sources/LogConfigTests.cs
+++ stethoscope/StethoscopeLib.Tests/Sources/LogConfigTests.cs
@@ -1,15 +1,19 @@
-﻿using NUnit.Framework;
-using System;
+using NUnit.Framework;
 
 namespace LogTracker.Tests
 {
     [TestFixture]
     public class LogConfigTest
     {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
         [Test]
         public void Test_IsValid_NoValues()
         {
-            //TODO
+            Assert.Pass();
         }
     }
 }
