dfac6603f287a2cb24cc4872cb67127725cab923
Rcmaniac25 <rcmaniac25@hotmail.com>
2018-04-13 02:08:09 -0400

    Updated unit tests to Nunit 3's preferred model

--- stethoscope/StethoscopeLib.Tests/Sources/LogConfigTests.cs
+++ stethoscope/StethoscopeLib.Tests/Sources/LogConfigTests.cs
@@ -2,9 +2,10 @@ using NUnit.Framework;
 
 namespace LogTracker.Tests
 {
-    [TestFixture]
+    [TestFixture(TestOf = typeof(LogConfig))]
     public class LogConfigTest
     {
+        // TODO: remove once we get a test that actually requires setup
         [SetUp]
         public void Setup()
         {
@@ -16,52 +17,42 @@ namespace LogTracker.Tests
         }
 
         [Test]
-        public void Test_IsValid_NoValues()
+        public void IsValidDefault()
         {
             var config = new LogConfig();
 
-            Assert.IsFalse(config.IsValid);
+            Assert.That(config.IsValid, Is.False);
         }
 
         [Test]
-        public void Test_IsValid_UnusedValueSet()
+        public void IsValidOtherValues()
         {
             var config = new LogConfig();
 
-            config.LogLinePath = "path";
+            config.LogLinePath = "i'm not used";
 
-            Assert.IsFalse(config.IsValid);
+            Assert.That(config.IsValid, Is.False);
         }
 
-        [Test]
-        public void Test_IsValid_TimestampSet()
+        // Pairwise/combinatorial would be useful, but I need to check the expected result value
+        // XXX add null values
+        [TestCase("", "", ExpectedResult = false)]
+        [TestCase("    ", "", ExpectedResult = false)]
+        [TestCase("", "    ", ExpectedResult = false)]
+        [TestCase("    ", "    ", ExpectedResult = false)]
+        [TestCase("time", "", ExpectedResult = false)]
+        [TestCase("", "log", ExpectedResult = false)]
+        [TestCase("time", "    ", ExpectedResult = false)]
+        [TestCase("    ", "log", ExpectedResult = false)]
+        [TestCase("time", "log", ExpectedResult = true)]
+        public bool IsValidValuesTest(string timestamp, string logMessage)
         {
             var config = new LogConfig();
 
-            config.TimestampPath = "path";
+            config.TimestampPath = timestamp;
+            config.LogMessagePath = logMessage;
 
-            Assert.IsFalse(config.IsValid);
-        }
-
-        [Test]
-        public void Test_IsValid_LogMessageSet()
-        {
-            var config = new LogConfig();
-
-            config.LogMessagePath = "path";
-
-            Assert.IsFalse(config.IsValid);
-        }
-
-        [Test]
-        public void Test_IsValid_TimestampAndLogMessageSet()
-        {
-            var config = new LogConfig();
-
-            config.TimestampPath = "path";
-            config.LogMessagePath = "path";
-
-            Assert.IsTrue(config.IsValid);
+            return config.IsValid;
         }
     }
 }
