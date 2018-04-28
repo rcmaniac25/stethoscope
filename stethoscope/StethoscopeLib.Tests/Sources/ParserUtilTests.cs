cef69349b843c2c4596d18a1dee0f84718171379
Rcmaniac25 <rcmaniac25@hotmail.com>
2018-04-28 02:55:34 -0400

    Started working on ParserUtil.CastField tests

--- stethoscope/StethoscopeLib.Tests/Sources/ParserUtilTests.cs
+++ stethoscope/StethoscopeLib.Tests/Sources/ParserUtilTests.cs
@@ -7,10 +7,29 @@ namespace LogTracker.Tests
     [TestFixture(TestOf = typeof(ParserUtil))]
     public class ParserUtilTests
     {
+        [TestCase(ParserPathElementFieldType.Unknown, ExpectedResult = null)]
+        [TestCase(ParserPathElementFieldType.NotAValue, ExpectedResult = null)]
+        public object InvalidCastType(ParserPathElementFieldType castType)
+        {
+            return ParserUtil.CastField("true", castType);
+        }
+
+        [TestCase("", ExpectedResult = null)]
+        [TestCase("true", ExpectedResult = true)]
+        [TestCase("false", ExpectedResult = false)]
+        [TestCase("TRUE", ExpectedResult = true)]
+        [TestCase("FALSE", ExpectedResult = false)]
+        [TestCase("True", ExpectedResult = true)]
+        [TestCase("False", ExpectedResult = false)]
+        [TestCase("TrUe", ExpectedResult = true)]
+        [TestCase("FaLsE", ExpectedResult = false)]
+        public object CastBool(string boolCast)
+        {
+            return ParserUtil.CastField(boolCast, ParserPathElementFieldType.Bool);
+        }
+
         //TODO: CastField
 
-        //TODO: ParseFieldType
-
-        //TODO: ParsePath
+        //TODO: ParsePath (and ParseFieldType)
     }
 }
