3b6ebc2b38a5af0d88062a45df0cdc6b052bdb70
Rcmaniac25 <rcmaniac25@hotmail.com>
2018-05-10 01:45:28 -0400

    Create a test file for XMLLogParser.

---
+++ stethoscope/StethoscopeLib.Tests/Sources/XMLLogParserTests.cs
@@ -0,0 +1,19 @@
+﻿using LogTracker.Parsers;
+using LogTracker.Parsers.Internal.XML;
+
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogTracker.Tests.Sources
+{
+    [TestFixture(TestOf = typeof(XMLLogParser))]
+    public class XMLLogParserTests
+    {
+        //TODO: SetConfig
+
+        //TODO: Parse
+    }
+}
