5a0bddc030ca9fe0616cb7120378b2fd7ecc285c
Rcmaniac25 <rcmaniac25@hotmail.com>
2018-06-09 02:32:03 -0400

    Kickoff work for IRegistryStorage

---
+++ stethoscope/StethoscopeLib/Sources/LogComponents/Internal/IRegistryStorage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reactive.Linq;
+
+using Stethoscope.Common;
+
+namespace Stethoscope.Log.Internal
+{
+    public interface IRegistryStorage
+    {
+        IQbservable<ILogEntry> Entries { get; }
+
+        int Count { get; }
+
+        Guid AddLog(ILogEntry entry);
+        //XXX: update?
+        void Clear();
+
+        //XXX: close/shutdown?
+    }
+}
