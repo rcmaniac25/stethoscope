383fff2ca7ebd80fb3e83552ec4212e346ab0a14
Rcmaniac25 <rcmaniac25@hotmail.com>
2018-08-31 03:04:15 -0400

    Updated TODOs for ObservableTests

--- stethoscope/StethoscopeLib.Tests/Sources/ObservableTests.cs
+++ stethoscope/StethoscopeLib.Tests/Sources/ObservableTests.cs
@@ -7,10 +7,36 @@ namespace Stethoscope.Tests
     [TestFixture]
     public class ObservableTests
     {
-        //TODO: IList observables
-
-        //TODO: IBaseListCollection observables
+        //TODO: ToObservables
 
         //TODO: GetObservableType
+
+        //TODO: basic tests with regular schedulers and long running schedulers
+
+        //--- Finite ---
+
+        //TODO: exception
+
+        //TODO: no messages at all
+
+        //TODO: a couple messages
+
+        //TODO: insert into list (non-threaded)
+
+        //TODO: insert into list (threaded)
+
+        //--- Infinite ---
+
+        //TODO: exception
+
+        //TODO: no messages at all
+
+        //TODO: a couple messages
+
+        //TODO: insert into list (non-threaded)
+
+        //TODO: insert into list (threaded)
+
+        //TODO: something that produces a CancellationDisposable for use cancelable
     }
 }
