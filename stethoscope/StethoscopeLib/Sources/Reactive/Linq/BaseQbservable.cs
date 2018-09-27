972bc799132ac436fe9b893459711cccca73dacb
Rcmaniac25 <rcmaniac25@hotmail.com>
2018-09-27 00:54:35 -0400

    Status (a plan)

---
+++ stethoscope/StethoscopeLib/Sources/Reactive/Linq/BaseQbservable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Stethoscope.Sources.Reactive.Linq
+{
+    internal class BaseQbservable<T> : IQbservable<T>
+    {
+        public Type ElementType => typeof(T);
+
+        public Expression Expression => throw new NotImplementedException();
+
+        public IQbservableProvider Provider => throw new NotImplementedException();
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class BaseQbservableProvider : IQbservableProvider
+    {
+        public IQbservable<TResult> CreateQuery<TResult>(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
