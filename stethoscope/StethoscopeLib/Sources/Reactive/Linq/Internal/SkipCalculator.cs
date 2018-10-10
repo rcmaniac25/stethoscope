681767987df3d548bcd3afcda1de6164aeaf6a1a
Rcmaniac25 <rcmaniac25@hotmail.com>
2018-10-10 07:21:16 -0400

    Initial handling of cases where skip would be invalidated

--- stethoscope/StethoscopeLib/Sources/Reactive/Linq/Internal/SkipCalculator.cs
+++ stethoscope/StethoscopeLib/Sources/Reactive/Linq/Internal/SkipCalculator.cs
@@ -26,11 +26,27 @@ namespace Stethoscope.Reactive.Linq.Internal
         {
             if (expression.Method.Name == "Skip")
             {
-                if (skipDepth < 0)
+                // Only specific skips can be used
+                if (expression.Arguments[1].Type == typeof(int))
                 {
-                    skipDepth = 0;
+                    if (skipDepth < 0)
+                    {
+                        skipDepth = 0;
+                    }
+                    skipDepth += ExpressionTreeHelpers.GetValueFromExpression<int>(expression.Arguments[1]);
                 }
-                skipDepth += ExpressionTreeHelpers.GetValueFromExpression<int>(expression.Arguments[1]);
+            }
+            else if (skipDepth >= 0 && expression.Arguments.Count > 1)
+            {
+                // If any function takes a lambda, there's some programatic element that can't skip a specific amount
+                for (int i = 1; i < expression.Arguments.Count; i++)
+                {
+                    if (expression.Arguments[i].NodeType == ExpressionType.Lambda)
+                    {
+                        skipDepth = -1;
+                        break;
+                    }
+                }
             }
 
             Visit(expression.Arguments[0]);
