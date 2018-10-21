a9129bead0099d6a8cd1fc7a36582c5043ce9f59
Rcmaniac25 <rcmaniac25@hotmail.com>
2018-10-21 02:57:17 -0400

    Changed skip tree modifier to a virtual state machine. Experimental...

--- stethoscope/StethoscopeLib/Sources/Reactive/Linq/Internal/SkipTreeModifier.cs
+++ stethoscope/StethoscopeLib/Sources/Reactive/Linq/Internal/SkipTreeModifier.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Linq;
 
@@ -6,77 +7,157 @@ namespace Stethoscope.Reactive.Linq.Internal
 {
     internal class SkipTreeModifier : ExpressionVisitor
     {
-        private Stack<MethodCallExpression> expressionsToSave;
+        private enum VisitStage
+        {
+            Uninitialized,
+
+            Stage1,
+            Stage2,
+            Stage3,
+            Done
+        }
+
+        private Expression finalExpression;
+        private VisitStage stage = VisitStage.Uninitialized;
+        private List<MethodCallExpression> methodCalls;
 
         public override Expression Visit(Expression node)
         {
-            expressionsToSave = null;
-            return base.Visit(node);
+            if (stage != VisitStage.Uninitialized)
+            {
+                throw new InvalidOperationException($"{nameof(Visit)} is already doing a calculation. Create a new instance for each calculation you want to perform.");
+            }
+            Reset();
+            finalExpression = node;
+
+            while (stage != VisitStage.Done)
+            {
+                ExecuteStages(node);
+            }
+
+            stage = VisitStage.Uninitialized;
+
+            return finalExpression;
+        }
+
+        private void Reset()
+        {
+            stage = VisitStage.Stage1;
+            methodCalls = new List<MethodCallExpression>();
         }
 
         protected override Expression VisitMethodCall(MethodCallExpression expression)
         {
-            var returnChild = false;
-            var expressionsToSaveOriginallyExisted = expressionsToSave != null; //TODO
-
-            if (expression.Method.Name == "Skip")
+            if (stage == VisitStage.Stage1)
             {
-                // Only specific skips can be used
-                if (expression.Arguments[1].Type == typeof(int))
-                {
-                    returnChild = true;
-                    if (expressionsToSave == null)
-                    {
-                        expressionsToSave = new Stack<MethodCallExpression>();
-                        expressionsToSaveOriginallyExisted = true;
-                    }
-                }
-                else if (expressionsToSave != null)
-                {
-                    expressionsToSave = null;
-                }
+                return VisitMethodCallStage1(expression);
             }
-            else if (expressionsToSave != null && expression.Arguments.Count > 1)
+            else if (stage == VisitStage.Stage3)
             {
-                // If any function takes a lambda, there's some programatic element that can't skip a specific amount
-                for (int i = 1; i < expression.Arguments.Count; i++)
-                {
-                    if (expression.Arguments[i].NodeType == ExpressionType.Lambda || expression.Arguments[i].NodeType == ExpressionType.Quote)
-                    {
-                        expressionsToSave = null;
-                        break;
-                    }
-                }
+                return VisitMethodCallStage3(expression);
             }
 
-            if (expressionsToSave != null)
+            throw new InvalidOperationException($"Unknown stage: {stage}");
+        }
+
+        private void ExecuteStages(Expression expression)
+        {
+            if (stage == VisitStage.Stage1)
             {
-                if (returnChild)
+                Stage1(expression);
+                if (methodCalls.Count > 0)
                 {
-                    expressionsToSave.Push(null);
+                    stage = VisitStage.Stage2;
                 }
                 else
                 {
-                    expressionsToSave.Push(expression);
+                    stage = VisitStage.Done;
                 }
             }
-
-            var child = base.Visit(expression.Arguments[0]);
-
-            if (expressionsToSave != null)
+            else if (stage == VisitStage.Stage2)
             {
-                if (expressionsToSave.Count > 0)
+                Stage2(expression);
+                if (methodCalls.Count > 0)
                 {
-                    var exp = expressionsToSave.Pop();
-                    if (exp != null)
+                    stage = VisitStage.Stage3;
+                }
+                else
+                {
+                    stage = VisitStage.Done;
+                }
+            }
+            else if (stage == VisitStage.Stage3)
+            {
+                Stage3(expression);
+                stage = VisitStage.Done;
+            }
+            else
+            {
+                throw new InvalidOperationException($"Unknown stage: {stage}");
+            }
+        }
+
+        private void Stage1(Expression expression)
+        {
+            base.Visit(expression);
+        }
+
+        private Expression VisitMethodCallStage1(MethodCallExpression expression)
+        {
+            methodCalls.Add(expression);
+            base.Visit(expression.Arguments[0]);
+            return expression;
+        }
+
+        private void Stage2(Expression expression)
+        {
+            int countSkipsFrom = 0;
+            for (int i = 0; i < methodCalls.Count; i++)
+            {
+                var method = methodCalls[i];
+                if (method.Method.Name == "Skip" && method.Arguments[1].Type != typeof(int))
+                {
+                    countSkipsFrom = i + 1;
+                }
+                else if (method.Arguments.Count > 1)
+                {
+                    for (int a = 1; a < method.Arguments.Count; a++)
                     {
-                        return exp;
+                        if (method.Arguments[a].NodeType == ExpressionType.Lambda || method.Arguments[a].NodeType == ExpressionType.Quote)
+                        {
+                            countSkipsFrom = i + 1;
+                            break;
+                        }
                     }
                 }
-                if (expressionsToSaveOriginallyExisted)
+            }
+            if (countSkipsFrom < methodCalls.Count)
+            {
+                for (int i = 0; i < countSkipsFrom; i++)
                 {
-                    return child;
+                    methodCalls[i] = null;
                 }
+            }
+            else
+            {
+                methodCalls.Clear();
+            }
+        }
+
+        private void Stage3(Expression expression)
+        {
+            finalExpression = base.Visit(expression);
+        }
+
+        private Expression VisitMethodCallStage3(MethodCallExpression expression)
+        {
+            var child = base.Visit(expression.Arguments[0]);
+            if (expression.Method.Name == "Skip" && expression.Arguments[1].Type == typeof(int))
+            {
+                return child;
+            }
+            if (child != expression.Arguments[0])
+            {
                 return Expression.Call(expression.Method, new Expression[] { child }.Concat(expression.Arguments.Skip(1)));
             }
             return expression;
